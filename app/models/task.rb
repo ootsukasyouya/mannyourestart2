00861711ab4c53f8c64db56cbc79eea3bdd8d218
ootsukasyouya <rikkuhose@gmail.com>
2021-12-04 12:50:36 +0900

    step2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ApplicationRecord
+  validates :content, presence: true
+  validates :title, presence: true
+  # validates :deadline, presence: true
+  # validates :status, presence: true
+  # enum status: {未着手:0,着手中:1,完了:2},_prefix: true
+  # enum priority: {高:0,中:1,低:2},_prefix: true
+  #
+  # scope :search_title,->(title_search){where("title LIKE?","%#{title_search}%")}
+  # scope :search_status,->(status_search){where(status:"#{status_search}")}
+  # def self.search(keyword)
+  #   if keyword != nil
+  #     where("title like?", "%#{keyword}%")
+  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,22 @@ module Taskunz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+     config.i18n.default_locale = :ja
+     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.generators do |g|
+    g.test_framework :rspec,
+      model_specs: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+     request_specs: false
+  end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
