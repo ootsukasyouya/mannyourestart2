61fa96073a80a107324f9125f1091b015d1658f5
ootsukasyouya <rikkuhose@gmail.com>
2021-12-05 01:27:19 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'users#new'
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
