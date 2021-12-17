0464e21303eb76e95ab69985876702a68edffaa3
ootsukasyouya <rikkuhose@gmail.com>
2021-12-18 02:29:49 +0900

    seedseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Label.create!([
-  { name: '仕事' },
-  { name: '私用' },
-  { name: '会食' },
-  { name: '実家' },
-  { name: '家族' },
-  { name: '子供' },
-  { name: '運動' },
-  { name: '資格' },
-  { name: 'テスト' },
-  { name: '料理' },
-  { name: 'ペット' }
-])
-User.create!(
-   email: 'te@test.com',
-   name: 'テスト太郎',
-   name: 'admin',
-   password: '123456',
-   admin: true
-)
+# Label.create!([
+#   { name: '仕事' },
+#   { name: '私用' },
+#   { name: '会食' },
+#   { name: '実家' },
+#   { name: '家族' },
+#   { name: '子供' },
+#   { name: '運動' },
+#   { name: '資格' },
+#   { name: 'テスト' },
+#   { name: '料理' },
+#   { name: 'ペット' }
+# ])
+# User.create!(
+#    email: 'te@test.com',
+#    name: 'テスト太郎',
+#    name: 'admin',
+#    password: '123456',
+#    admin: true
+# )
 10.times do |n|
   User.create!(
     email: "test#{n + 1}@test.com",
@@ -32,11 +32,9 @@ User.create!(
     password: '12345',
   )
 end
-10.times do |m|
   10.times do |n|
     Task.create!(
       title:"test_task#{n+1}",
       content:"test#{n+1}",
     )
   end
-end
