0ac4de85e087d473ffcb78dab90e39531df23d45
ootsukasyouya <rikkuhose@gmail.com>
2021-12-18 01:25:01 +0900

    show

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 User.create!(
    email: 'test@test.com',
    name: 'テスト太郎',
-   password: '12345',
+   password: '123456',
 )
 5.times do |n|
   User.create!(
     email: "test#{n + 1}@test.com",
     name: "test_user#{n + 1}",
-    password: '12345',
+    password: '123456',
   )
 end
 5.times do |m|
