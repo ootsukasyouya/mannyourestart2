edc528731805eb734f913ed63f8c786b485cfdcb
ootsukasyouya <rikkuhose@gmail.com>
2021-12-18 02:13:45 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-5.times do |n|
-  User.create!(
-    email: "test#{n + 1}@test.com",
-    name: "test_user#{n + 1}",
-    password: '123456',
-  )
-end
-5.times do |m|
-  5.times do |n|
-    Task.create!(
-      title:"test_task#{n+1}",
-      content:"test#{n+1}",
-      status:"未着手",
-      deadline:"2022-01-01",
-      user_id: m+2
-    )
-  end
-end
-
 Label.create!([
   { name: '仕事' },
   { name: '私用' },
@@ -38,3 +18,25 @@ Label.create!([
   { name: '料理' },
   { name: 'ペット' }
 ])
+User.create!(
+   email: 'test@test.com',
+   name: 'テスト太郎',
+   name: 'admin',
+   password: '123456',
+   admin: true
+)
+10.times do |n|
+  User.create!(
+    email: "test#{n + 1}@test.com",
+    name: "test_user#{n + 1}",
+    password: '12345',
+  )
+end
+10.times do |m|
+  10.times do |n|
+    Task.create!(
+      title:"test_task#{n+1}",
+      content:"test#{n+1}",
+    )
+  end
+end
